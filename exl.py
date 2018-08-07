a752f23e099aded8366c3399639b7be9dde8efe7
isyang <i.syang@foxmail.com>
2018-08-08 00:13:28 +0800

    print

---
+++ exl.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.5
+print "Hello World!"
+print "Hello Again"
+print "I like typing this ."
+print "This is fun."
+print "Yey! Printing."
+print "I'd much rather you not'."
+print 'I "Said "  do not touch this.'
